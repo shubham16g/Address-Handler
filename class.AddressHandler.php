336eed8325b9b0d9dd4a6ee9eaa1a0ac01e255f1
Shubham Gupta <shubham.developer16@gmail.com>
2021-04-13 09:15:30 +0530

    Real_Scape_String and more get methods

--- class.AddressHandler.php
+++ class.AddressHandler.php
@@ -24,7 +24,7 @@ class AddressHandler
         `district` VARCHAR(255) NOT NULL ,
         `postalLocation` VARCHAR(255) NOT NULL ,
         `createdAt` DATETIME NOT NULL ,
-        `extraJson` JSON NULL ,
+        `extras` JSON NULL ,
         `lat` DOUBLE NOT NULL default " . self::DEFAULT_LATLNG . " ,
         `lng` DOUBLE NOT NULL default " . self::DEFAULT_LATLNG . " ,
         PRIMARY KEY (`addressID`)
@@ -40,11 +40,17 @@ class AddressHandler
         $this->db = $db;
     }
 
-    public function addAddress(string $uid, string $postalCode, string  $countryCode, string  $state, string  $district, string  $postalLocation, array $extraJson = null, float $lat = self::DEFAULT_LATLNG, float $lng = self::DEFAULT_LATLNG)
+    public function addAddress(string $uid, string $postalCode, string  $countryCode, string  $state, string  $district, string  $postalLocation, array $extras = null, float $lat = self::DEFAULT_LATLNG, float $lng = self::DEFAULT_LATLNG)
     {
+        $this->_escape($postalCode);
+        $this->_escape($countryCode);
+        $this->_escape($state);
+        $this->_escape($district);
+        $this->_escape($postalLocation);
+        
         $currentTime = $this->_getCurrentTimeForMySQL();
-        $ej = ($extraJson != null) ? ("'" . json_encode($extraJson) . "'") : 'null';
-        $query = "INSERT INTO all_addresses (`uid`, `postalCode`, `countryCode`, `state`, `district`, `postalLocation`, `lat`, `lng`, `extraJson`, `createdAt`)
+        $ej = ($extras != null) ? ("'" . json_encode($extras) . "'") : 'null';
+        $query = "INSERT INTO all_addresses (`uid`, `postalCode`, `countryCode`, `state`, `district`, `postalLocation`, `lat`, `lng`, `extras`, `createdAt`)
         VALUES ('$uid', '$postalCode', '$countryCode', '$state', '$district', '$postalLocation', '$lat', '$lng', $ej, '$currentTime')";
 
         if (!$this->db->query($query)) {
@@ -54,7 +60,30 @@ class AddressHandler
 
     public function getAddressesByUid(string $uid)
     {
-        $query = "SELECT * FROM all_addresses WHERE `uid` = '$uid'";
+        return $this->_getAddresses('uid', $uid);
+    }
+
+    public function getAddressesByDistrict(string $district)
+    {
+        return $this->_getAddresses('district', $district);
+    }
+
+    public function getAddressesByState(string $state)
+    {
+        return $this->_getAddresses('state', $state);
+    }
+
+    public function getAddressByAddressID(int $addressID)
+    {
+        return $this->_getAddresses('addressID', $addressID)[0];
+    }
+
+    private function _getAddresses(string $key, string $value)
+    {
+        $this->_escape($key);
+        $this->_escape($value);
+
+        $query = "SELECT * FROM all_addresses WHERE `$key` = '$value'";
 
         $res = $this->db->query($query);
         if (!$res) {
@@ -62,7 +91,7 @@ class AddressHandler
         }
         $addresses = [];
         while ($row = $res->fetch_assoc()) {
-            $row['extraJson'] = (array) json_decode($row['extraJson']);
+            $row['extras'] = (array) json_decode($row['extras']);
             $addresses[] = $row;
         }
         return $addresses;
@@ -76,8 +105,13 @@ class AddressHandler
         }
     }
 
-    public function updateAddress(int $addressID, string $postalCode, string $countryCode, string $state, string $district, string  $postalLocation, array $extraJson = null, float $lat = self::DEFAULT_LATLNG, float $lng = self::DEFAULT_LATLNG)
+    public function updateAddress(int $addressID, string $postalCode, string $countryCode, string $state, string $district, string  $postalLocation, array $extras = null, float $lat = self::DEFAULT_LATLNG, float $lng = self::DEFAULT_LATLNG)
     {
+        $this->_escape($postalCode);
+        $this->_escape($countryCode);
+        $this->_escape($state);
+        $this->_escape($district);
+        $this->_escape($postalLocation);
         $query = "UPDATE all_addresses SET 
         `postalCode` = '$postalCode',
         `countryCode` = '$countryCode',
@@ -91,8 +125,8 @@ class AddressHandler
         if ($lng != self::DEFAULT_LATLNG) {
             $query .= ",`lng` = '$lng'";
         }
-        if ($extraJson != null) {
-            $query .= ",`extraJson` = '" . json_encode($extraJson) ."'";
+        if ($extras != null) {
+            $query .= ",`extras` = '" . json_encode($extras) ."'";
         }
         $query .= " WHERE `addressID` = '$addressID'";
         if (!$this->db->query($query)) {
@@ -103,5 +137,12 @@ class AddressHandler
     private function _getCurrentTimeForMySQL()
     {
         return date('Y-m-d H:i:s', time());
+    }
+
+    private function _escape(&$var)
+    {
+        $var = htmlspecialchars($var);
+        $var = $this->db->real_escape_string($var);
+        // $var = $this->db->real_escape_string($var);
     }
 }
